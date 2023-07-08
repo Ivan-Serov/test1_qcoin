2d81c8880f7d0f163a6feaeae8360406507326ff
Ivan Serov <van9i.serov1@gmail.com>
2023-07-08 16:12:20 +0300

    Изменил диаграммы, теперь, при наведении, отображаются проценетные соотношения парамметров. Диаграммы больше не апдейтятся, а удаляются и создаются заново

--- src/components/SensorChart/SensorChart.jsx
+++ src/components/SensorChart/SensorChart.jsx
@@ -5,10 +5,10 @@ import './SensorChart.css';
 
 const SensorChart = ({ sensorNumber }) => {
   const canvasRef = useRef(null); // Ссылка на DOM-элемент canvas
-  const chartRef = useRef(null); // Ссылка на экземпляр Chart.js
+  const chartRef = useRef(null);  // Ссылка на экземпляр Chart.js
   const [chartData, setChartData] = useState(null); // Состояние данных для графика
   // eslint-disable-next-line react-hooks/exhaustive-deps
-  const chartColors = ['#FF6384', '#36A2EB', '#FFCE56']; // Цвета для секторов графика
+  const chartColors = ['#FF6384', '#36A2EB', '#FFCE56', '#DDEE67']; // Цвета для секторов графика
   
   // Получение и перепаковка данных
   useEffect(() => {
@@ -16,22 +16,29 @@ const SensorChart = ({ sensorNumber }) => {
       try {
         const config = { headers: { 'Content-Type': 'application/x-www-form-urlencoded' } };
         const response = await axios.post('http://qcan.tactics.su/test_ivan.php?load_data', { sensor: sensorNumber }, config);
-        const data = response.data;
-        const response2 = await axios.get(`http://qcan.tactics.su/test_ivan.php?load_data&status`);
-        const data2 = response2.data;
+        const response2 = await axios.get('http://qcan.tactics.su/test_ivan.php?load_data&status');
 
-        if (data.status !== 1 ||  data2.status !== 1) {
+        if (response.data.status !== 1 || response2.data.status !== 1) {
           throw new Error('Ошибка получения данных');
         }
 
-        const labels = [];
-        const dataArr = [];
+        let data = response.data.answer;
+        let data2 = response2.data.answer;
 
-        for (const key in data2.answer) {
-          dataArr.push(data.answer[key]);
-          labels.push(data2.answer[key]);
-          
+        const total = Object.values(data).reduce((acc, value) => acc + value, 0);
+        const percentages = {};
+
+        const labels = [];
+
+        for (const key in data2) {
+          const value = data[key];
+          const percent = ((value / total) * 100).toFixed(2);
+          percentages[key] = parseFloat(percent);
+          labels.push(data2[key]);
         }
+
+        const dataArr = Object.keys(data2).map((key) => percentages[key]);
+
         setChartData({ labels, data: dataArr }); // Установка полученных данных в состояние
       } catch (error) {
         console.error(error);
@@ -46,49 +53,50 @@ const SensorChart = ({ sensorNumber }) => {
       clearInterval(fetchDataInterval); // Очистка интервала при размонтировании компонента
     };
   }, [sensorNumber]);
-  // Создание и отображение Диаграммы
+  
+  // Создание диаграммы
   useEffect(() => {
     if (chartData) {
-      if (!chartRef.current) { // Если экземпляр Chart.js не был создан
-        const ctx = canvasRef.current.getContext('2d'); // Получение контекста отрисовки для canvas
-        
-        const chartConfig = {
-          type: 'doughnut', // Тип графика - "донат"
-          data: {
-            labels: chartData.labels, // Метки для секторов графика
-            datasets: [
-              {
-                label: `Sensor ${sensorNumber}`, // Название датасета
-                data: chartData.data, // Данные для секторов графика
-                backgroundColor: chartColors,  // Цвета для секторов графика
-              },
-            ],
-          },
-          options: {
-            maintainAspectRatio: false, // Отключение автоподстройки размеров графика
-            plugins: {
-              tooltip: {
-                callbacks: {
-                  label: function (context) {
-                    return context.label + ': ' + context.parsed.toFixed(2) + '%'; // Форматирование текста всплывающей подсказки
-                  },
+      if (chartRef.current) {
+        chartRef.current.destroy(); // Удаление диаграммы, если она уже существует
+      }
+
+      const ctx = canvasRef.current.getContext('2d'); // Получение контекста отрисовки для canvas
+      
+      const chartConfig = {
+        type: 'doughnut', // Тип графика - "донат"
+        data: {
+          labels: chartData.labels, // Метки для секторов графика
+          datasets: [
+            {
+              label: `Sensor ${sensorNumber}`, // Название датасета
+              data: chartData.data, // Данные для секторов графика
+              backgroundColor: chartColors,  // Цвета для секторов графика
+            },
+          ],
+        },
+        options: {
+          maintainAspectRatio: false, // Отключение автоподстройки размеров графика
+          plugins: {
+            tooltip: {
+              callbacks: {
+                label: function (context) {
+                  const label = context.label;
+                  const percent = chartData.data[context.dataIndex];
+                  return `${label}: ${percent}%`; // Форматирование текста всплывающей подсказки
                 },
               },
             },
           },
-        };
+        },
+      };
 
-        chartRef.current = new Chart(ctx, chartConfig); // Создание нового экземлпяра Chart.js и сохранение ссылки
-        chartRef.current.update(); // Обновление графика
-      } else { // Если экземпляр Chart.js уже существует
-        chartRef.current.data.labels = chartData.labels; // Обновление меток секторов графика
-        chartRef.current.data.datasets[0].data = chartData.data; // Обновление данных секторов графика
-        chartRef.current.update(); // Обновление графика
-      }
+      chartRef.current = new Chart(ctx, chartConfig);
     }
   }, [chartColors, chartData, sensorNumber]);
 
-  return <canvas className="doughnut" ref={canvasRef} />; // Возвращение компонента canvas для отображения графика
+  return <canvas className="doughnut" ref={canvasRef} />;
 };
 
 export default SensorChart;
+
