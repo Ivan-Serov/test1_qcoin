42c2eac5302345f8f88ebfc1a7efebcf7f2007d1
Ivan Serov <van9i.serov1@gmail.com>
2023-07-08 19:35:29 +0300

    Вынес константы

--- src/components/SensorChart/SensorChart.jsx
+++ src/components/SensorChart/SensorChart.jsx
@@ -2,33 +2,28 @@ import React, { useEffect, useState, useRef } from 'react';
 import axios from 'axios';
 import Chart from 'chart.js/auto';
 import './SensorChart.css';
+import {  chartColors, config, percentages, } from '../../utils/constants'
 
 const SensorChart = ({ sensorNumber }) => {
   const canvasRef = useRef(null); // Ссылка на DOM-элемент canvas
   const chartRef = useRef(null);  // Ссылка на экземпляр Chart.js
   const [chartData, setChartData] = useState(null); // Состояние данных для графика
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  const chartColors = ['#FF6384', '#36A2EB', '#FFCE56', '#DDEE67']; // Цвета для секторов графика
   
   // Получение и перепаковка данных
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const config = { headers: { 'Content-Type': 'application/x-www-form-urlencoded' } };
         const response = await axios.post('http://qcan.tactics.su/test_ivan.php?load_data', { sensor: sensorNumber }, config);
         const response2 = await axios.get('http://qcan.tactics.su/test_ivan.php?load_data&status');
+        let data = response.data.answer;
+        let data2 = response2.data.answer;
+        
+        let total = Object.values(data).reduce((acc, value) => acc + value, 0);
+        let labels = [];
 
         if (response.data.status !== 1 || response2.data.status !== 1) {
           throw new Error('Ошибка получения данных');
         }
-
-        let data = response.data.answer;
-        let data2 = response2.data.answer;
-
-        const total = Object.values(data).reduce((acc, value) => acc + value, 0);
-        const percentages = {};
-
-        const labels = [];
 
         for (const key in data2) {
           const value = data[key];
@@ -37,7 +32,7 @@ const SensorChart = ({ sensorNumber }) => {
           labels.push(data2[key]);
         }
 
-        const dataArr = Object.keys(data2).map((key) => percentages[key]);
+        let dataArr = Object.keys(data2).map((key) => percentages[key]);
 
         setChartData({ labels, data: dataArr }); // Установка полученных данных в состояние
       } catch (error) {
@@ -95,7 +90,7 @@ const SensorChart = ({ sensorNumber }) => {
         chartRef.current.update(); // Обновление графика
       }
     }
-  }, [chartColors, chartData, sensorNumber]);
+  }, [chartData, sensorNumber]);
 
   return <canvas className="doughnut" ref={canvasRef} />; // Возвращение компонента canvas для отображения графика
 };
---
+++ src/utils/constants.js
@@ -0,0 +1,10 @@
+const chartColors = ['#FF6384', '#36A2EB', '#FFCE56', '#DDEE67']; // Цвета для секторов графика
+const config = { headers: { 'Content-Type': 'application/x-www-form-urlencoded' } };
+const percentages = {};
+
+
+export {
+    chartColors,
+    config,
+    percentages
+}
